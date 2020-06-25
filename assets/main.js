155a16386394941a01fd853fa0064cb97657ce94
Jeffrey Harding <jeffreyh202@gmail.com>
2020-06-24 23:49:05 -0600

    formatting and basic js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,33 @@
+// Call time of day
+var d = new Date();
 
+var currentHrs = d.getHours();
+console.log(currentHrs)
+// add Event listener to respond to time of day and change background color with if/else
+var cellColor = document.querySelectorAll('.cell');
+
+// cellColor.forEach(function(item) {
+//     item.addEventListener("click", function(){
+//       console.log("color");
+//       $(this).addClass("changeColor");
+//     })
+// })
+
+var CC = document.querySelector("#test");
+var cc = parseInt(document.querySelector("#test").getAttribute("value"));
+console.log(cc)
+    
+if(currentHrs >= cc) {
+        console.log("yes");
+        $("#test").addClass("changeColor");
+    }
+
+// cellColor.addEventListener('click', function(){
+//     console.log("color");
+//     $(this).addClass("changeColor");
+// });
+
+//make "edit" button open a text entry field in div
+var editBtn = document.querySelector(".edit-btn");
+
+//make save button appear with text entry box
