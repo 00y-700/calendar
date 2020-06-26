1ef98f145ddbce014b44fb541f427382a6a8a46b
Jeffrey Harding <jeffreyh202@gmail.com>
2020-06-25 22:01:02 -0600

    rough js

--- assets/main.js
+++ assets/main.js
@@ -10,13 +10,33 @@ var editBtn = document.querySelectorAll('.edit-btn');
 var CC = document.querySelector(".cell");
 var cc = parseInt(document.querySelector(".cell").getAttribute("value"));
 console.log(cc)
-var cc9 = parseInt(document.querySelector("#test").getAttribute("value"));
+var cc9 = parseInt(document.querySelector("#nine").getAttribute("value"));
+var cc10 = parseInt(document.querySelector("#ten").getAttribute("value"));
+var cc11 = parseInt(document.querySelector("#eleven").getAttribute("value"));
+var cc12 = parseInt(document.querySelector("#twelve").getAttribute("value"));
+var cc1 = parseInt(document.querySelector("#one").getAttribute("value"));
+var cc2 = parseInt(document.querySelector("#two").getAttribute("value"));
+var cc3 = parseInt(document.querySelector("#three").getAttribute("value"));
+var cc4 = parseInt(document.querySelector("#four").getAttribute("value"));
+var cc5 = parseInt(document.querySelector("#five").getAttribute("value"));
 // Change Cell Color based on time
-if(currentHrs >= cc) {
+if(currentHrs >= cc9) {
     var edt = "true";
-    $(cellColor).addClass("changeColor");
+    $("#nine").addClass("changeColor");
 } else {
     var edt = "false"};
+
+if(currentHrs >= cc10) {
+    var edt = "true";
+    $("#ten").addClass("changeColor");
+} else {
+    var edt = "false"};
+        
+if(currentHrs >= cc11) {
+        var edt = "true";
+        $("#eleven").addClass("changeColor");
+} else {
+        var edt = "false"};
 
 //Make Cell Editable
 var changeText = $(editBtn).on('click', textEdit);
