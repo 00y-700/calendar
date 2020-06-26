a958d9dbb02cd0cb15977f6189bdb2c6956f34de
Jeffrey Harding <jeffreyh202@gmail.com>
2020-06-25 21:52:38 -0600

    js cleanup

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,34 @@
 // Call time of day
 var d = new Date();
 
-var currentHrs = d.getHours();
+var currentHrs = 10;
+// d.getHours();
 console.log(currentHrs)
 // add Event listener to respond to time of day and change background color with if/else
 var cellColor = document.querySelectorAll('.cell');
-
-// cellColor.forEach(function(item) {
-//     item.addEventListener("click", function(){
-//       console.log("color");
-//       $(this).addClass("changeColor");
-//     })
-// })
-
-var CC = document.querySelector("#test");
-var cc = parseInt(document.querySelector("#test").getAttribute("value"));
+var editBtn = document.querySelectorAll('.edit-btn');
+var CC = document.querySelector(".cell");
+var cc = parseInt(document.querySelector(".cell").getAttribute("value"));
 console.log(cc)
-    
+var cc9 = parseInt(document.querySelector("#test").getAttribute("value"));
+// Change Cell Color based on time
 if(currentHrs >= cc) {
-        console.log("yes");
-        $("#test").addClass("changeColor");
-    }
+    var edt = "true";
+    $(cellColor).addClass("changeColor");
+} else {
+    var edt = "false"};
 
-// cellColor.addEventListener('click', function(){
-//     console.log("color");
-//     $(this).addClass("changeColor");
-// });
+//Make Cell Editable
+var changeText = $(editBtn).on('click', textEdit);
 
-//make "edit" button open a text entry field in div
-var editBtn = document.querySelector(".edit-btn");
+function textEdit() {
+    if(edt === "false") {
+    $(cellColor).attr("contentEditable", "true");
+    $(cellColor).click();
+    };
 
-//make save button appear with text entry box
+};
+
+
+
+
